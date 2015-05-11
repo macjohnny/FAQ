acc73d2ed1c925efd9b251a066c3b31303bb4c6b
Esteban Marín <estebanmarin@gmx.ch>
2015-05-11 16:48:40 +0200

    Update PublicFAQGet.pm

--- Kernel/GenericInterface/Operation/FAQ/PublicFAQGet.pm
+++ Kernel/GenericInterface/Operation/FAQ/PublicFAQGet.pm
@@ -261,8 +261,8 @@ sub Run {
                         FileID => $Attachment->{FileID},
                         UserID => $UserID,
                     );
-                    $File{FileID} = $Attachment->{FileID}
-                    $File{Inline} = $Attachment->{Inline}
+                    $File{FileID} = $Attachment->{FileID};
+                    $File{Inline} = $Attachment->{Inline};
                     # convert content to base64
                     $File{Content} = encode_base64( $File{Content} );
                 }
